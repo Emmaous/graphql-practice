50b941e117615ab8536200655658e015e6cc0593
Emmaous <emmaogah97@gmail.com>
2020-06-30 10:43:20 +0100

    Created and Linked the mongodb database Created mutations for adding data to db Moved data to db Resolved resolve functions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+mongoose .connect(
+    "mongodb+srv://Avatah:blue2583@graphql-avatah-upcfd.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+   )
+   .then(() => console.log("Connected to MongoDB Atlas"))
+   .catch(err => console.log("Error: ", err.message));
 
 const app = express();
 

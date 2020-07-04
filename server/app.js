a10a4f56690aca60256390a32339ee06c003056e
Emmaous <emmaogah97@gmail.com>
2020-07-04 22:03:16 +0100

    changed server port

--- server/app.js
+++ server/app.js
@@ -17,6 +17,6 @@ app.use('/graphql',graphqlHTTP({
     graphiql: true
 }));
 
-app.listen(3000, () => {
-    console.log("now listening on port 3000")
+app.listen(4000, () => {
+    console.log("now listening on port 4000")
 })
